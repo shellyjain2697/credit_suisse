41008fef1e5cc8fae3b3388b9e572b9f43db8eda
shellyjain2697 <sgadia97@gmail.com>
2021-11-08 16:57:55 +0530

    initial commit

---
+++ src/main/java/com/repository/AlertRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import com.model.persistence.Alert;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends CrudRepository<Alert, String> {
+}
